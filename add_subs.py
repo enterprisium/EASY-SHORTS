16a1b5551a12a0ce53b17da258fac323ef343e59
Muhammad Shakeel <137326535+enterprisium@users.noreply.github.com>
2024-07-12 07:27:45 -0700

    Update add_subs.py

--- add_subs.py
+++ add_subs.py
@@ -2,8 +2,8 @@ from faster_whisper import WhisperModel
 import json
 from moviepy.editor import *
 import numpy as np
-from moviepy.config import change_settings
-change_settings({"IMAGEMAGICK_BINARY": r"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\\magick.exe"})
+#from moviepy.config import change_settings
+#change_settings({"IMAGEMAGICK_BINARY": r"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\\magick.exe"})
 
 def split_text_into_lines(data):
 
