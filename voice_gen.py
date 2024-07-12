d746111f95453e00876cd13f2bf3a54976313bf2
Muhammad Shakeel <137326535+enterprisium@users.noreply.github.com>
2024-07-12 07:23:01 -0700

    Update voice_gen.py

--- voice_gen.py
+++ voice_gen.py
@@ -1,9 +1,10 @@
 import os
 import elevenlabs
 
-elevenlabs_api_key = os.getenv('ELEVENLABS_API_KEY')
-elevenlabs.set_api_key(elevenlabs_api_key)
+# Set the API key as an environment variable
+os.environ["ELEVENLABS_API_KEY"] = "sk_45fe7923b58a44371ab2667045e1e9b50e40b8250d4b2b24" 
 
+# Now you can use the ElevenLabs library
 def generate_voice_over(script):
     voice = elevenlabs.Voice(
         voice_id = "ErXwobaYiN019PkySvjV",
