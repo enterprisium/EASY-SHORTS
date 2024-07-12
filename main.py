cdc72dd483d16f5e7fb70c71728af525155119a0
Muhammad Shakeel <137326535+enterprisium@users.noreply.github.com>
2024-07-12 07:08:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from video_make import *
 from add_subs import *
 
 if __name__ == "__main__":
-    niche = input("choose your niche eg. 'making money'")
+    niche = input("choose your niche eg. 'making money': ")
     ideas = gpt_entry(niche)
     ideas = [x for x in ideas if x != ""]
     for part in ideas:
         print(part)
     while True:
-        choice = input(f'pick your desired video (from 1 to {len(ideas)})')
-        if choice >= 1 and choice <= len(ideas):
+        choice = int(input(f'pick your desired video (from 1 to {len(ideas)}): '))
+        if 1 <= choice <= len(ideas):
             break
         else:
-            print(f'wrong input, type in value in beetween 1 and {len(ideas)}')
-    script = gpt_reformat(ideas[int(choice) - 1])
+            print(f'wrong input, type in value between 1 and {len(ideas)}')
+    script = gpt_reformat(ideas[choice - 1])
     generate_voice_over(script)
     make_vid(niche)
     add_subtitles('voice_over.mp3')
